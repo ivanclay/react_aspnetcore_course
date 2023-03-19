1263d488eceeac2ffdcce4398c19c86d6aad7169
Ivan Clay <ivan.clay.moura@gmail.com>
2023-03-19 17:42:44 -0300

    use first hook, useState

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -1,16 +1,49 @@
+import { useState } from 'react';
 import './App.css';
 
+let initialState = [
+  {
+    id: 1,
+    descricao: 'Primeira Atividade'
+  },
+  {
+    id: 2,
+    descricao: 'Segunda Atividade'
+  }
+]
+
 function App() {
+
+  const [atividades, setAtividades] = useState(initialState)
+
+  function addAtividade(e){
+    e.preventDefault();
+    
+    const novaAtividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value
+    }
+
+    atividades.push(novaAtividade);
+    setAtividades([...atividades]);
+    console.log(atividades);
+  }
+
   return (
-    <div className="mt-3">
-       <ul className='list-group'>
-        <li className='list-group-item'>Item 1</li>
-        <li className='list-group-item'>Item 1</li>
-        <li className='list-group-item'>Item 1</li>
-        <li className='list-group-item'>Item 1</li>
-        <li className='list-group-item'>Item 1</li>
-        </ul>
-    </div>
+    <>
+      <form>
+        <input id='id' placeholder='id' />
+        <input id='descricao' placeholder='descricao' />
+        <button onClick={addAtividade}>+ Atividade</button>
+      </form>
+      <div className="mt-3">
+        <ul className='list-group'>
+          {atividades.map(ativ => (
+            <li className='list-group-item' key={ativ.id}>{ativ.id}-{ativ.descricao}</li>
+          ))}
+          </ul>
+      </div>
+    </>
   );
 }
 
