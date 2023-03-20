41c45acbdf0c2acb9af8d9d9d5d7d81343ccdd82
Ivan Clay <ivan.clay.moura@gmail.com>
2023-03-20 08:16:14 -0300

    add fontawesome-free, use bootstrap card

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -29,18 +29,55 @@ function App() {
 
   return (
     <>
-      <form>
-        <input id='id' placeholder='id' />
-        <input id='descricao' placeholder='descricao' />
-        <button onClick={addAtividade}>+ Atividade</button>
+      <form className="row g-3">
+          <div className="col-md-6">
+            <label className="form-label">Id</label>
+            <input id='id' placeholder='id' type="text" className="form-control"/>
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Descrição</label>
+            <input id='descricao' placeholder='descricao' type="text" className="form-control"/>
+          </div>
+          <hr/>
+          <div className="col-12">
+            <button 
+              className="btn btn-outline-secondary" 
+              onClick={addAtividade}>
+                + Atividade
+            </button>
+          </div>
       </form>
       <div className="mt-3">
-        <ul className='list-group'>
           {atividades.map(ativ => (
-            <li className='list-group-item' key={ativ.id}>{ativ.id}-{ativ.descricao}</li>
+            <div className="card mb-2 shadow-sm" key={ativ.id}>
+             
+              <div className="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 className='card-title'>
+                  <span 
+                    className="badge bg-secondary me-1">
+                    {ativ.id}
+                  </span>
+                  - Título
+                </h5>
+                <h6>
+                  Prioridade: 
+                    <span className='text-black ms-1'>
+                      <i className='me-1 far fa-meh'></i>
+                      Normal
+                    </span>
+                </h6>
+              </div>
+                <p className="card-text">
+                  {ativ.descricao}
+                </p>
+              </div>
+          </div>
           ))}
-          </ul>
       </div>
+
+     
+
     </>
   );
 }
